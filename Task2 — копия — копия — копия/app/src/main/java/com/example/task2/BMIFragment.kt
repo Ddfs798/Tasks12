0e9fb84fd68643471d7052b3b82452eb7afac280
Ddfs798 <colorvvvv@gmail.com>
2023-07-04 21:31:47 +0300

    home work 2

---
+++ Task2 — копия — копия — копия/app/src/main/java/com/example/task2/BMIFragment.kt
@@ -0,0 +1,8 @@
+package com.example.task2
+
+import androidx.fragment.app.Fragment
+
+class BMIFragment : Fragment(R.layout.fragment_bmi) {
+
+
+}
---
+++ Task2 — копия — копия — копия/app/src/main/java/com/example/task2/СaloriesFragment.kt
@@ -0,0 +1,7 @@
+package com.example.task2
+
+import androidx.fragment.app.Fragment
+
+class СaloriesFragment : Fragment(R.layout.fragment_calories) {
+
+}
